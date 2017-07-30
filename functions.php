c27ea72a0b8f1d368c54c7b243feb74852a76e0b
Tony Barry <TonyMPBarry@gmail.com>
2017-07-30 11:56:35 -0700

    Adds wysiwyg editor removal on specified page templates

--- functions.php
+++ functions.php
@@ -57,6 +57,34 @@
 
 
   /**
+  * Remove wysywig editor from template pages
+  */
+  function remove_editor_from_templates()
+  {
+    if ( !is_admin() ) { return; }
+
+    if ( !isset( $_GET['post'] ) ) { return; }
+
+    /* basename is used for templates that are in the subdirectory of the theme */
+    $current_page_template_slug = basename( get_page_template_slug( $_GET['post'] ) );
+    var_dump( $current_page_template_slug );
+
+    /* file names of templates to remove the editor on */
+    $excluded_template_slugs = [
+      'home.php'
+    ];
+
+    if ( in_array( $current_page_template_slug, $excluded_template_slugs ) )
+    {
+      /* remove editor from pages */
+      remove_post_type_support( 'page', 'editor' );
+    }
+
+  }
+  add_action( 'init', 'remove_editor_from_templates' );
+
+
+  /**
   * Enqueue scripts and styles.
   */
   function chadkamer_enqueue()
